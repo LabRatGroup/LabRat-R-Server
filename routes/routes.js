ed06f10ebd959b74e3adf835ed29b8bb9fc92970
Julio M. Fernandez <jfernandez74@gmail.com>
2018-04-23 23:09:57 +0200

    Added log comment and params

--- routes/routes.js
+++ routes/routes.js
@@ -7,8 +7,10 @@ var appRouter = function (app) {
     app.get("/train/:token", function (req, res) {
         var sh = require('shelljs');
         var token = req.params.token;
+
+        console.log('Processing model training data for '+token);
         console.log(sh.exec('pwd'));
-        var command = sh.exec(['rscript', './generateModel.R', token].join(' ')).stdout;
+        var command = sh.exec(['rscript', './generateModel.R', token].join(' '));
 
         res.status(200).send(command);
     });

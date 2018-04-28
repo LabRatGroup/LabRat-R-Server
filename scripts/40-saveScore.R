b11382a3a2636bf908f516714e18d7845d321f74
Julio M. Fernandez <jfernandez74@gmail.com>
2018-04-29 00:38:02 +0200

    Swithed token for state data token

--- scripts/40-saveScore.R
+++ scripts/40-saveScore.R
@@ -1,8 +1,3 @@
-generateToken <- function(n = 25) {
-  a <- do.call(paste0, replicate(n, sample(LETTERS, 1, TRUE), FALSE))
-  paste0(a, sprintf("%04d", sample(9999, 1, TRUE)), sample(LETTERS, 1, TRUE))
-}
-
 filename <- file.path(sprintf("%s/models",dir),sprintf("%s.rda", token))
 saveRDS(data.model, file=filename)
 
@@ -10,7 +5,7 @@ insertSql_1 <- "INSERT INTO `ml_model_state_scores` "
 insertSql_2 <- "(`results`, `token`, `ml_model_state_id`, `params`, `kappa`, `accuracy`, `confusion_matrix`, `sensitivity`, `specificity`, `precision`, `recall`, `created_at`, `updated_at`, `deleted_at`) values "
 insertSql_3 <- sprintf("('%s','%s',%s,'%s',%s ,%s ,'%s', %s, %s, %s, %s,'%s','%s',NULL)", 
                        toJSON(data.model$results),
-                       generateToken(), 
+                       data.query$token, 
                        data.query$ml_model_state_id, 
                        data.query$params, 
                        data.confusionMatrix$overall['Kappa'][[1]],

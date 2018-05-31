5eb292a10af0de62f86a6dd3c8c187b95a53f54a
Julio M. Fernandez <jfernandez74@gmail.com>
2018-05-31 15:32:05 +0200

    Added mongo env var

--- scripts/45-mongoDataSave.R
+++ scripts/45-mongoDataSave.R
@@ -1,6 +1,6 @@
 #print('############### Saving pedictions');
-
-mongoCon <- mongo(collection = "ml_model_prediction_score", db = "labrat")
+mongoUrl = sprintf("mongodb://%s:%s@127.0.0.1:%s/labrat", data.env$mongoUser, data.env$mongoPassword, data.env$mongoPort)
+mongoCon <- mongo(collection = "ml_model_prediction_score", url = mongoUrl)
 mongoCon$remove(sprintf('{"ml_model_prediction_data_id" : %s}', data.query$id))
 mongoCon$count()
 mongoCon$insert(as.data.frame(data.prediction))
